f1d1b9b880b420d2f210463dd38b1402e4ca35b0
Gary Webb <gary@thecloudandcompass.com>
2018-08-23 21:22:31 +0100

    Fix Issue #3 bug: Nullable Property Types don't work Nullable's underlying type used to define property's type instead of StructureMap inferring the type from the property directly

---
+++ src/CherryPickerTests/TestClasses/NullableTypeClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CherryPickerTests.TestClasses
+{
+    public class NullableTypeClass
+    {
+        public int? NullableInt { get; set; }
+    }
+}
